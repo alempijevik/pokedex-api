9ab77c8805ad93407025ad94c056dadac1878f4e
FLuff <filip@scitoperformance.com>
2022-02-26 08:39:06 +0100

    initial commit

---
+++ app/Console/Commands/PopulatePokemonTable.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use App\Models\Pokemon;
+
+
+class PopulatePokemonTable extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'populate:pokemon_table';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $file = File::get(storage_path('app/pokedex.json'));
+        collect(json_decode($file, true))->each(function ($pokemon) {
+                Pokemon::create([
+                    'name' => $pokemon['name'],
+                    'stats' => $pokemon['stats'],
+                    'desc1' => $pokemon['desc1']
+                ]);
+            });
+    }
+}
---
+++ app/Console/Commands/PopulateTypesTable.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Type;
+use Illuminate\Console\Command;
+
+class PopulateTypesTable extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'populate:types_table';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $types = ['Normal', 'Fire', 'Water', 'Grass', 'Electric', 'Ice', 'Fighting', 'Poison', 'Ground', 'Flying', 'Psychic', 'Bug', 'Rock', 'Ghost', 'Dark', 'Dragon', 'Steel', 'Fairy'];
+        
+        collect($types)->each(function ($type) {
+                Type::create([
+                    'name' => $type,
+                ]);
+            });
+    }
+}
